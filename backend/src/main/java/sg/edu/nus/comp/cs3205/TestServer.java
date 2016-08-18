2da14c050b17e5c4cef7bc7ff86f65fc9fd558c5
Joshua Lee <me@jolly.sg>
2016-08-18 13:46:26 +0800

    added json example

--- backend/src/main/java/sg/edu/nus/comp/cs3205/TestServer.java
+++ backend/src/main/java/sg/edu/nus/comp/cs3205/TestServer.java
@@ -1,13 +1,26 @@
 package sg.edu.nus.comp.cs3205;
 
-import static spark.Spark.*;
+import com.google.gson.Gson;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class TestServer {
 
     public static void main(String[] args) {
         port(8080);
+
         get("/", (request, response) -> "Hello World!");
+
         get("/echo/:echo", (request, response) -> "Echo: " + request.params(":echo"));
+
+        Gson gson = new Gson();
+        get("/json/:json", (request, response) -> {
+            String params = request.params(":json");
+            TestObject testObject = new TestObject();
+            testObject.setTestString(params);
+            return gson.toJson(testObject);
+        });
     }
 
 }
