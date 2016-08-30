571f7875b0ea0f845ba4396e39c72eaddb0bca86
Joshua Lee <me@jolly.sg>
2016-08-30 23:27:21 +0800

    merged testobject into testserver

--- backend/src/main/java/sg/edu/nus/comp/cs3205/TestServer.java
+++ backend/src/main/java/sg/edu/nus/comp/cs3205/TestServer.java
@@ -23,4 +23,22 @@ public class TestServer {
         });
     }
 
+    private static class TestObject {
+
+        private String testString;
+        private String testString1;
+        private String testString2;
+        private String testString3;
+        private String testString4;
+
+        void setTestString(String testString) {
+            this.testString = testString;
+            this.testString1 = testString + "1";
+            this.testString2 = testString + "2";
+            this.testString3 = testString + "3";
+            this.testString4 = testString + "4";
+        }
+
+    }
+
 }
