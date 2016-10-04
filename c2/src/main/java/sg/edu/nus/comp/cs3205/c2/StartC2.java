e3a05f527afad5e9d8ab925107a39e775b068a3f
Joshua Lee <me@jolly.sg>
2016-10-04 18:31:56 +0800

    added c2 project

---
+++ c2/src/main/java/sg/edu/nus/comp/cs3205/c2/StartC2.java
@@ -0,0 +1,14 @@
+package sg.edu.nus.comp.cs3205.c2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StartC2 {
+
+    private static Logger logger = LoggerFactory.getLogger(StartC2.class.getSimpleName());
+
+    public static void main(String[] args) {
+        logger.info("Starting C2");
+    }
+
+}
