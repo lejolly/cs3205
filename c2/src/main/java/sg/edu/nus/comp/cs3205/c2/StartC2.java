b3589d36ad29d17559644ecefaff7d8604779a7d
Chunqi Zhu <chunqi93@gmail.com>
2016-10-12 02:35:56 +0800

    Added command line arguments to C2
    * Usage: StartC2 <tcp/udp> <port>
    * Added JAR manifest essentials, task :jar now produces runnable JAR
    file in /lib

--- c2/src/main/java/sg/edu/nus/comp/cs3205/c2/StartC2.java
+++ c2/src/main/java/sg/edu/nus/comp/cs3205/c2/StartC2.java
@@ -2,17 +2,27 @@ package sg.edu.nus.comp.cs3205.c2;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import sg.edu.nus.comp.cs3205.c2.network.NetworkManager;
 
 public class StartC2 {
-
-    private static Logger logger = LoggerFactory.getLogger(StartC2.class.getSimpleName());
-    
-    private static NetworkManager networkManager;
+    private static final Logger LOGGER = LoggerFactory.getLogger(StartC2.class);
+    private static final String USAGE = "Usage: StartC2 <tcp/udp> <port>";
     
     public static void main(String[] args) {
-        logger.info("Starting C2");
-        networkManager = new NetworkManager();
+        if(args.length != 2 || (!args[0].equals("tcp") && !args[0].equals("udp"))) {
+            LOGGER.error(USAGE);
+        } else {
+            String type = args[0];
+            try {
+                int port = Integer.parseInt(args[1]);
+                LOGGER.info("starting C2 " + type + " on port " + port);
+                new NetworkManager(port);
+            } catch (NumberFormatException e) {
+                LOGGER.error("invalid port number");
+                LOGGER.error(USAGE);
+            }
+        }
     }
 
 }
--- c2/src/main/java/sg/edu/nus/comp/cs3205/c2/network/NetworkManager.java
+++ c2/src/main/java/sg/edu/nus/comp/cs3205/c2/network/NetworkManager.java
@@ -1,37 +1,33 @@
 package sg.edu.nus.comp.cs3205.c2.network;
 
+import java.security.Key;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.security.Key;
 
 public class NetworkManager {
-
     private static Logger logger = LoggerFactory.getLogger(NetworkManager.class.getSimpleName());
-
-    private static final int SERVER_PORT = 8081;
 
     public Key key = null;
     private EventLoopGroup bossGroup;
     private EventLoopGroup workerGroup;
 
-    public NetworkManager() {
+    public NetworkManager(int port) {
         logger.info("Initializing network manager.");
         bossGroup = new NioEventLoopGroup(1);
         workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .handler(new LoggingHandler(LogLevel.INFO))
-                    .childHandler(new ServerChannelInitializer(this));
-            b.bind(SERVER_PORT).sync().channel().closeFuture().sync();
+            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO)).childHandler(new ServerChannelInitializer(this));
+            b.bind(port).sync().channel().closeFuture().sync();
         } catch (InterruptedException e) {
             logger.error("InterruptedException: ", e);
         }
