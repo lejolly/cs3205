1370ede700616e77956757738ccd1a4de65943f3
Joshua Lee <me@jolly.sg>
2016-10-17 15:11:53 +0800

    added support for reading of keys

--- backend/src/main/java/sg/edu/nus/comp/cs3205/common/keys/KeyGenerator.java
+++ backend/src/main/java/sg/edu/nus/comp/cs3205/common/keys/KeyGenerator.java
@@ -1,61 +1,34 @@
 package sg.edu.nus.comp.cs3205.common.keys;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sg.edu.nus.comp.cs3205.common.data.keys.PemFile;
+import sg.edu.nus.comp.cs3205.common.utils.KeyUtils;
 
 import java.io.IOException;
-import java.security.*;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.spec.InvalidKeySpecException;
 
-// https://www.txedo.com/blog/java-generate-rsa-keys-write-pem-file/
 public class KeyGenerator {
 
     private static final Logger logger = LoggerFactory.getLogger(KeyGenerator.class.getSimpleName());
 
     public static void main(String[] args) {
         try {
-            KeyPairGenerator keyPairGenerator = initialiseKeyPairGenerator();
-            generateAndWriteKeyPairs("c1", keyPairGenerator);
-            generateAndWriteKeyPairs("c2", keyPairGenerator);
-            generateAndWriteKeyPairs("c3", keyPairGenerator);
+            KeyPairGenerator keyPairGenerator = KeyUtils.initialiseKeyPairGenerator();
+            KeyUtils.generateAndWriteKeyPairs("c1", keyPairGenerator);
+            KeyUtils.generateAndWriteKeyPairs("c2", keyPairGenerator);
+            KeyUtils.generateAndWriteKeyPairs("c3", keyPairGenerator);
         } catch (NoSuchAlgorithmException e) {
             logger.error("NoSuchAlgorithmException: ", e);
         } catch (NoSuchProviderException e) {
             logger.error("NoSuchProviderException: ", e);
         } catch (IOException e) {
             logger.error("IOException: ", e);
+        } catch (InvalidKeySpecException e) {
+            logger.error("InvalidKeySpecException: ", e);
         }
-    }
-
-    private static void generateAndWriteKeyPairs(String keyPairName, KeyPairGenerator keyPairGenerator)
-            throws IOException {
-        logger.info("Generating keys for " + keyPairName);
-        KeyPair keyPair = keyPairGenerator.generateKeyPair();
-        RSAPrivateKey rsaPrivateKey = (RSAPrivateKey) keyPair.getPrivate();
-        RSAPublicKey rsaPublicKey = (RSAPublicKey) keyPair.getPublic();
-        writePemFile(rsaPrivateKey, "RSA PRIVATE KEY", "keys/" + keyPairName + "_id_rsa");
-        writePemFile(rsaPublicKey, "RSA PUBLIC KEY", "keys/" + keyPairName + "_id_rsa.pub");
-    }
-
-    private static KeyPairGenerator initialiseKeyPairGenerator()
-            throws NoSuchAlgorithmException, NoSuchProviderException {
-        logger.info("Initialising key pair generator.");
-        Security.addProvider(new BouncyCastleProvider());
-        logger.info("BouncyCastle provider added.");
-        SecureRandom random = SecureRandom.getInstanceStrong();
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
-        keyPairGenerator.initialize(2048, random);
-        return keyPairGenerator;
-    }
-
-    private static void writePemFile(Key key, String description, String filename)
-            throws IOException {
-        PemFile pemFile = new PemFile(key, description);
-        pemFile.write(filename);
-        logger.info(String.format("%s successfully writen in file %s.", description, filename));
     }
 
 }
---
+++ backend/src/main/java/sg/edu/nus/comp/cs3205/common/utils/KeyUtils.java
@@ -0,0 +1,96 @@
+package sg.edu.nus.comp.cs3205.common.utils;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+// https://www.txedo.com/blog/java-generate-rsa-keys-write-pem-file/
+// https://www.txedo.com/blog/java-read-rsa-keys-pem-file/
+public class KeyUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(KeyUtils.class.getSimpleName());
+
+    private static final String RSA_PUBLIC_KEY_TYPE = "RSA PUBLIC KEY";
+    private static final String RSA_PRIVATE_KEY_TYPE = "RSA PRIVATE KEY";
+
+    public static void checkAndAddBouncyCastleProvider() {
+        if (Security.getProvider("BC") == null) {
+            Security.addProvider(new BouncyCastleProvider());
+            logger.info("BouncyCastle provider added.");
+        }
+    }
+
+    public static KeyPairGenerator initialiseKeyPairGenerator()
+            throws NoSuchAlgorithmException, NoSuchProviderException {
+        logger.info("Initialising key pair generator.");
+        checkAndAddBouncyCastleProvider();
+        SecureRandom random = SecureRandom.getInstanceStrong();
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
+        keyPairGenerator.initialize(2048, random);
+        return keyPairGenerator;
+    }
+
+    public static void generateAndWriteKeyPairs(String keyPairName, KeyPairGenerator keyPairGenerator)
+            throws IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException {
+        logger.info("Generating keys for " + keyPairName);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        RSAPrivateKey rsaPrivateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAPublicKey rsaPublicKey = (RSAPublicKey) keyPair.getPublic();
+        writePemFile(rsaPrivateKey, RSA_PRIVATE_KEY_TYPE, "keys/" + keyPairName + "_id_rsa");
+        writePemFile(rsaPublicKey, RSA_PUBLIC_KEY_TYPE, "keys/" + keyPairName + "_id_rsa.pub");
+    }
+
+    public static void writePemFile(Key key, String description, String filename)
+            throws IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException {
+        PemObject pemObject = new PemObject(description, key.getEncoded());
+        try (PemWriter pemWriter = new PemWriter(new OutputStreamWriter(new FileOutputStream(filename)))) {
+            pemWriter.writeObject(pemObject);
+        }
+        logger.info(String.format("%s successfully writen in file %s.", description, filename));
+        if (verifyKey(filename, key)) {
+            logger.info(String.format("%s successfully verified.", description));
+        } else {
+            logger.warn(String.format("%s unable to be verified.", description));
+        }
+    }
+
+    public static Key readPemFile(String filename)
+            throws IOException, NoSuchProviderException, NoSuchAlgorithmException, InvalidKeySpecException {
+        checkAndAddBouncyCastleProvider();
+        KeyFactory factory = KeyFactory.getInstance("RSA", "BC");
+        PEMParser pemParser = new PEMParser(new FileReader(filename));
+        PemObject pemObject = (PemObject) pemParser.readPemObject();
+        if (pemObject.getType().equals(RSA_PRIVATE_KEY_TYPE)) {
+            byte[] content = pemObject.getContent();
+            PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(content);
+            return factory.generatePrivate(privKeySpec);
+        } else if (pemObject.getType().equals(RSA_PUBLIC_KEY_TYPE)) {
+            byte[] content = pemObject.getContent();
+            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(content);
+            return factory.generatePublic(pubKeySpec);
+        }
+        return null;
+    }
+
+    public static boolean verifyKey(String filename, Key key)
+            throws IOException, NoSuchProviderException, NoSuchAlgorithmException, InvalidKeySpecException {
+        logger.info(String.format("Verifying %s", filename));
+        Key readKey = readPemFile(filename);
+        return key.equals(readKey);
+    }
+
+}
