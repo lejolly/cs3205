1f6554044566b208be10f690c225a4d309e951f5
Joshua Lee <me@jolly.sg>
2016-11-04 18:09:29 +0800

    removed old database methods

--- backend/src/main/java/sg/edu/nus/comp/cs3205/c3/database/C3DatabaseManager.java
+++ backend/src/main/java/sg/edu/nus/comp/cs3205/c3/database/C3DatabaseManager.java
@@ -7,8 +7,6 @@ import sg.edu.nus.comp.cs3205.common.data.config.DBCredentialsConfig;
 import sg.edu.nus.comp.cs3205.common.utils.JsonUtils;
 
 import java.sql.*;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Optional;
 
 public class C3DatabaseManager extends AbstractManager {
@@ -57,48 +55,6 @@ public class C3DatabaseManager extends AbstractManager {
     private Optional<DBCredentialsConfig> readDBCredentialsFromFile() {
         logger.info("Reading database credentials from file.");
         return JsonUtils.readJsonFile("config/db-credentials.json", DBCredentialsConfig.class);
-    }
-
-    public static int getActorCount() {
-        try {
-            logger.info("Getting number of actors");
-            Statement statement = dbConnection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM actor");
-            if (resultSet.next()) {
-                logger.info("Number of actors: " + resultSet.getInt(1));
-                return resultSet.getInt(1);
-            } else {
-                logger.warn("Unable to get number of actors.");
-            }
-        } catch (SQLException e) {
-            logger.error("SQLException: ", e);
-        }
-        return 0;
-    }
-
-    public static String getActorInfo(int id) {
-        try {
-            logger.info("Getting info of actor id: " + id);
-            PreparedStatement preparedStatement =
-                    dbConnection.prepareStatement("SELECT *  FROM actor WHERE actor_id=?");
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                logger.info("Info of actor id " + id + ": " + resultSet.getString(2) + ", " + resultSet.getString(3)
-                        + ", " + resultSet.getString(4));
-                Map<String, String> map = new LinkedHashMap<>();
-                map.put("id", resultSet.getString(1));
-                map.put("first_name", resultSet.getString(2));
-                map.put("last_name", resultSet.getString(3));
-                map.put("last_update", resultSet.getString(4));
-                return JsonUtils.toJsonString(map);
-            } else {
-                logger.warn("Unable to get number of actors.");
-            }
-        } catch (SQLException e) {
-            logger.error("SQLException: ", e);
-        }
-        return "";
     }
 
     public Connection getDbConnection() {
