640e2e80e24c2c2114864b8ec633e58dc102d74d
Chunqi Zhu <chunqi93@gmail.com>
2016-11-07 03:07:31 +0800

    Added retrieve request to C1
    * Retrieve request without a row_id specified returns all rows within
    table

--- frontend/application/libraries/Auth.php
+++ frontend/application/libraries/Auth.php
@@ -3,9 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 @session_start();
 class Auth {
-	public static function check_auth() {
+	public function check_auth() {
 		if(!isset($_SESSION['auth_token'])) {
 			redirect('session/login');
 		}
+	}
+
+	public function check_admin() {
+		$this->check_auth();
+		
+	}
+
+	public function get_auth_token() {
+		return $_SESSION['auth_token'];
 	}
 }
